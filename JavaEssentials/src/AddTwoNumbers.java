e55f82bc590f0a87827193e117778daf692cf27f
openl <openl@LAPTOP-I19TL4UC.fios-router.home>
2021-01-23 17:03:42 -0500

    Added a class to add two numbers

---
+++ JavaEssentials/src/AddTwoNumbers.java
@@ -0,0 +1,12 @@
+
+public class AddTwoNumbers {
+
+	public static void main(String[] args) {
+		int a = 10; int b = 20; int c;
+		c = a +b;
+		System.out.println("Result = " +c);
+		System.out.println("Hello World!!. This is my first Java program");
+
+	}
+
+}
